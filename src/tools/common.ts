daf3ffaaaa5126b5633bdef040799c257fab0a27
Petr Broz <petr.broz@autodesk.com>
2025-03-19 15:21:31 +0100

    Added token caching.

--- src/tools/common.ts
+++ src/tools/common.ts
@@ -10,7 +10,18 @@ export interface Tool<Args extends ZodRawShape> {
     callback: ToolCallback<Args>;
 }
 
+const credentialsCache = new Map<string, { accessToken: string, expiresAt: number }>();
+
 export async function getAccessToken(scopes: string[]): Promise<string> {
-    const credentials = await getServiceAccountAccessToken(APS_CLIENT_ID, APS_CLIENT_SECRET, APS_SA_ID, APS_SA_KEY_ID, APS_SA_PRIVATE_KEY, scopes);
-    return credentials.access_token;
+    const cacheKey = scopes.join("+");
+    let credentials = credentialsCache.get(cacheKey);
+    if (!credentials || credentials.expiresAt < Date.now()) {
+        const { access_token, expires_in } = await getServiceAccountAccessToken(APS_CLIENT_ID, APS_CLIENT_SECRET, APS_SA_ID, APS_SA_KEY_ID, APS_SA_PRIVATE_KEY, scopes);
+        credentials = {
+            accessToken: access_token,
+            expiresAt: Date.now() + expires_in * 1000
+        };
+        credentialsCache.set(cacheKey, credentials);
+    }
+    return credentials.accessToken;
 }
