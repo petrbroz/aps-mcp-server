26f11b00cebcb9c562e192ae9b47344666087fe2
Petr Broz <petr.broz@autodesk.com>
2025-06-17 09:31:40 +0200

    Don't rely on process.cwd when reading the .env file.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
 import path from "node:path";
+import url from "node:url";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 const { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_SA_ID, APS_SA_EMAIL, APS_SA_KEY_ID, APS_SA_PRIVATE_KEY } = process.env;
 
 export {
