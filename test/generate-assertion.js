4c1e7f9461022f72f4be90d5de98da5f3a2d6680
Petr Broz <petr.broz@autodesk.com>
2025-09-24 15:33:42 +0200

    Updated assertion generator test.

--- test/generate-assertion.js
+++ test/generate-assertion.js
@@ -1,26 +1,32 @@
+import fs from "node:fs/promises";
 import jwt from "jsonwebtoken";
 import { APS_CLIENT_ID, APS_SA_ID, APS_SA_KEY_ID, APS_SA_PRIVATE_KEY } from "../build/config.js";
+
+const LOG_FILE = "generate-assertion.log";
+async function log(message, data) {
+    const logEntry = { timestamp: new Date().toISOString(), message, data };
+    await fs.appendFile(LOG_FILE, JSON.stringify(logEntry) + "\n");
+}
 
 async function generateAssertion() {
     const payload = {
         iss: APS_CLIENT_ID,
         sub: APS_SA_ID,
         aud: "https://developer.api.autodesk.com/authentication/v2/token",
-        exp: 1758720617, // Math.floor(Date.now() / 1000) + 300, // 5 minutes
+        exp: 1758721100, // Math.floor(Date.now() / 1000) + 300, // 5 minutes
         scope: "data:read"
     };
     const options = {
         algorithm: "RS256",
         header: { alg: "RS256", kid: APS_SA_KEY_ID }
     };
-    console.log({ payload, options, privateKey: APS_SA_PRIVATE_KEY });
+    await log("Generating assertion", { payload, options, privateKey: APS_SA_PRIVATE_KEY });
     return jwt.sign(payload, APS_SA_PRIVATE_KEY, options);
 }
 
-console.log("APS_CLIENT_ID:", APS_CLIENT_ID);
 generateAssertion()
     .then((assertion) => {
-        console.log({ assertion });
+        return log("Generated assertion", { assertion });
     })
     .catch((err) => {
         console.error("Error generating assertion:", err);
